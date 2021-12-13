e3dfd48817ffab96c0eaef73156045d6af4f5662
crouchr <richard.crouch100@gmail.com>
2021-12-13 13:35:25 +0000

    Updates

--- app/get_env_app.py
+++ app/get_env_app.py
@@ -52,6 +52,16 @@ def get_vane_height_m():
 #     return float(snow_prob_level)
 
 
+# fudge factor used by CRHUDA rain prediction algorithm
+def get_rain_k_factor():
+    if 'RAIN_K_FACTOR' in os.environ:
+        rain_k_factor = os.environ['RAIN_K_FACTOR']
+    else:
+        rain_k_factor = 0.167
+
+    return rain_k_factor
+
+
 # elevation in metres
 def get_site_elevation():
     if 'SITE_ELEVATION' in os.environ:
--- app/meteod.py
+++ app/meteod.py
@@ -31,10 +31,13 @@ def main():
         topic = get_env_app.get_mqttd_topic()
         poll_secs = get_env_app.get_poll_secs()
         window_len = get_env_app.get_window_len()
+
         pressure_smoothed = moving_averages.MovingAverage(window_len)
         sea_level_pressure_smoothed = moving_averages.MovingAverage(window_len)
         temperature_smoothed = moving_averages.MovingAverage(window_len)
         humidity_smoothed = moving_averages.MovingAverage(window_len)
+        s1_m_avg = moving_averages.MovingAverage(window_len)
+        s2_m_avg = moving_averages.MovingAverage(window_len)
 
         print('MQTT broker IP : ' + broker)
         print(f'MQTT broker port : {port}')
@@ -45,6 +48,7 @@ def main():
         client1 = paho.Client("control1")  # create client object
         # client1.on_publish = on_publish                          #assign function to callback
         client1.connect(broker, port)
+        s2_avg_last = 0
 
         # Get the raw data from the Met sensor
         hum_sensor, press_sensor, temperature_sensor, status_msg = meteo2_sensor.register_meteo2_sensor()
@@ -57,9 +61,12 @@ def main():
             vane_height_m = get_env_app.get_vane_height_m()
             site_elevation_m = float(get_env_app.get_site_elevation())
             sensor_elevation_m = site_elevation_m + vane_height_m
+            rain_k_factor = get_env_app.get_rain_k_factor()
+
             print(f'site_elevation_m : {site_elevation_m}')
             print(f'vane_height_m : {vane_height_m}')
             print(f'sensor_elevation_m : {sensor_elevation_m}') # sensor elevation
+            print(f'rain_k_factor : {rain_k_factor}')
 
             # Read raw data from sensors
             humidity, pressure, temperature = meteo2_sensor.get_meteo_values(hum_sensor, press_sensor, temperature_sensor)
@@ -69,6 +76,37 @@ def main():
             dew_point_c = dew_point.get_dew_point(temperature, humidity)
             wet_bulb_c = wet_bulb.get_wet_bulb(temperature, pressure, dew_point_c)
             cloud_base_ft = cloud_base.calc_cloud_base_ft(temperature, dew_point_c)
+
+            # CRHUDA model https://www.researchgate.net/publication/337236701_Algorithm_to_Predict_the_Rainfall_Starting_Point_as_a_Function_of_Atmospheric_Pressure_Humidity_and_Dewpoint
+            # This metric calculation should be moved OUT of cloudmetricsd
+            if dew_point_c < 1.0:
+                crhuda_dew = 1.0
+            else:
+                crhuda_dew = dew_point_c
+            s1 = humidity
+            # s2_raw = round(pressure / crhuda_dew, 1)
+            s2 = round(rain_k_factor * (pressure / crhuda_dew), 1)
+
+            if s2 > 150.0:
+                s2 = 150.0
+
+            s1_m_avg.add(s1)
+            s2_m_avg.add(s2)
+
+            s1_avg = s1_m_avg.get_moving_average()
+            s2_avg = s2_m_avg.get_moving_average()
+
+            # s2 slope -
+            s2_delta = s2_avg - s2_avg_last
+            s2_avg_last = s2_avg
+
+            # add this metric in here once good values have been determined in a downstream daemon
+            # in phase waiting for S2 to increase and cross S1
+            # if s2_avg < s1_avg:
+            #     crhuda_primed = 10.0
+            # else:
+            #     crhuda_primed = 0.0
+            # end of CRHUDA
 
             # Smoothed data
             pressure_smoothed.add(pressure)
@@ -94,12 +132,16 @@ def main():
             metrics['dew_point'] = dew_point_c
             metrics['wet_bulb'] = wet_bulb_c
             metrics['cloud_base_ft'] = cloud_base_ft
+            metrics['crhuda_primed'] = -10 # To be added in the future
 
             # smoothed data
             metrics['temp_c_smoothed'] = temperature_smoothed.get_moving_average()
             metrics['humidity_smoothed'] = humidity_smoothed.get_moving_average()
             metrics['pressure_abs_smoothed'] = pressure_smoothed.get_moving_average()
             metrics['pressure_sea_smoothed'] = sea_level_pressure_smoothed.get_moving_average()
+            metrics['crhuda_s1'] = s1_avg
+            metrics['crhuda_s2'] = s2_avg
+            metrics['crhuda_s2_delta'] = s2_delta
 
             MQTT_MSG = json.dumps(metrics)
             pprint(metrics)
