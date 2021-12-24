1d9ac44faa3372928addc4114fb614d8ac0ae99f
crouchr <richard.crouch100@gmail.com>
2021-12-24 00:09:50 +0000

    Updates

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # based on Debian Buster
 FROM python:3.8.5-buster
 LABEL author="Richard Crouch"
-LABEL description="Meteo Daemon"
+LABEL description="Meteod & Yoctopuce Metv2 Daemon"
 
 # generate logs in unbuffered mode
 ENV PYTHONUNBUFFERED=1
---
+++ cicd/build_pi_docker_image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+VERSION=1.0.0
+
+cd ..
+#docker build --no-cache -t cicd:meteod .
+#docker tag cicd:meteod registry:5000/meteod:$VERSION
+#docker push registry:5000/meteod:$VERSION
+#docker rmi cicd:meteod
+
+docker build --no-cache -t meteod .
+docker tag meteod meteod:$VERSION
+#docker push registry:5000/meteod:$VERSION
+#docker rmi cicd:meteod
